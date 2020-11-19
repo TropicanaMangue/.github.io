fda93391599dc1f6558cf425c307ce2b97d5cee4
Sylvestre LEGROS <legros.sylvestre@gmail.com>
2020-11-19 23:27:55 +0100

    Exercice 3 Done

--- Exercices/src/app/app.component.ts
+++ Exercices/src/app/app.component.ts
@@ -6,4 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  displayText = false;
+  counter = 0;
+  logger = [];
+
+  toggleDisplay() {
+    this.displayText = !this.displayText;
+    // this.logger[this.counter] = ++this.counter;
+    this.logger.push(Date());
+  }
 }
